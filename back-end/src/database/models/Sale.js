0a5a9a8737243f33255a305dfbcb8a23c27c7933
Aron Adams <aronrapetto@hotmail.com>
2022-11-29 17:41:36 -0300

    build: Sale model

---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,53 @@
+const saleModel = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    sellerId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    totalPrice: {
+      type: DataTypes.DECIMAL(9, 2),
+    },
+    deliveryAddress: {
+      type: DataTypes.STRING(100),
+    },
+    deliveryNumber: {
+      type: DataTypes.STRING(50),
+    },
+    saleDate: {
+      type: DataTypes.DATE,
+    },
+    status: {
+      type: DataTypes.STRING(50),
+    },
+  }, {
+    tableName: 'sales',
+    underscored: true,
+    createdAt: 'sale_date',
+    updatedAt: false,
+  });
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user'
+    });
+
+    Sale.belongsTo(models.User, {
+      foreignKey: 'sellerId',
+      as: 'seller'
+    });
+  }
+
+  return Sale
+}
+
+module.exports = saleModel;
