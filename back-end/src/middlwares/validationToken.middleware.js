f2f942d0fec430477f300143e9fc2cb1bfaa67f0
Aron Adams <aronrapetto@hotmail.com>
2022-11-30 20:08:42 -0300

    build: Joi dependencie

--- back-end/src/middlwares/validationToken.middleware.js
+++ back-end/src/middlwares/validationToken.middleware.js
@@ -4,13 +4,13 @@ const validationToken = (req, res, next) => {
   const { originalUrl } = req;
   const { authorization } = req.headers;
 
+  // New validation
   if (!authorization && originalUrl === '/user/register') return next();
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
 
   const user = verifyToken(authorization);
   req.user = { ...user };
-  next();
-  
+  next();  
 }
 
 module.exports = validationToken;
