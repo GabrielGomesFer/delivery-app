ca87ca0863ce657d482768ebce90f3e6fabe8d62
Aron Adams <aronrapetto@hotmail.com>
2022-11-30 18:13:58 -0300

    feat: route POST /login

---
+++ back-end/src/service/login.service.js
@@ -0,0 +1,20 @@
+const { User } = require('../database/models');
+const { generateToken } = require('../utils/JWT');
+const md5 = require('md5');
+const errorThrower = require('../utils/errorThrower');
+
+const login = async ({ email, password }) => {
+  const encodedPassword = md5(password);
+  const user = await User.findOne({ where: { email, password: encodedPassword } });
+
+  if (!user) errorThrower(404, 'Incorrect email or password');
+
+  const token = generateToken(user);
+
+  return token;
+};
+
+module.exports = {
+  login,
+} 
+  
