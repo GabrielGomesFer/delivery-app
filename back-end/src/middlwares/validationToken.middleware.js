9ccc872b51da42ca7a4b89a84bb93d48253229a2
Aron Adams <aronrapetto@hotmail.com>
2022-11-30 19:55:23 -0300

    feat: validationToken.middleware

---
+++ back-end/src/middlwares/validationToken.middleware.js
@@ -0,0 +1,16 @@
+const { verifyToken } = require("../utils/JWT");
+
+const validationToken = (req, res, next) => {
+  const { originalUrl } = req;
+  const { authorization } = req.headers;
+
+  if (!authorization && originalUrl === '/user/register') return next();
+  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+  const user = verifyToken(authorization);
+  req.user = { ...user };
+  next();
+  
+}
+
+module.exports = validationToken;
