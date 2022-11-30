eedb7b0dfc42cc97945671d9a3a1bf9a1ea2a3c4
Aron Adams <aronrapetto@hotmail.com>
2022-11-30 20:25:58 -0300

    fix: register user validation

--- back-end/src/service/login.service.js
+++ back-end/src/service/login.service.js
@@ -4,7 +4,6 @@ const md5 = require('md5');
 const errorThrower = require('../utils/errorThrower');
 
 const login = async ({ email, password }) => {
-  // Validation of email and password
   const encodedPassword = md5(password);
   const user = await User.findOne({ where: { email, password: encodedPassword } });
 
--- back-end/src/service/user.service.js
+++ back-end/src/service/user.service.js
@@ -1,14 +1,19 @@
 const md5 = require('md5');
+const { Op } = require('sequelize');
 const { User } = require('../database/models');
 const errorThrower = require('../utils/errorThrower');
 
 const register = async ({ name, email, password, role='customer' }, user) => {
-  // Validation of name, email and password;
   // New validation
   if (user && user.role !== 'administrator') errorThrower(401, 'You shall not pass!');
 
   const encodedPassword = md5(password);
-  const isAlredyUserExist = await User.findOne({ where: { email } });
+  const isAlredyUserExist = await User.findOne({ where: {
+    [Op.or]: [
+      { email },
+      { name },
+    ]
+  } });
 
   // New validation
   if (isAlredyUserExist) errorThrower(409, 'User alredy registered');
