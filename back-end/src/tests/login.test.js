f3e8afde5955e9b282998bdd1fb4d4a719e07928
GabrielGomesFer <gabriel.ferreira2002@hotmail.com>
2022-12-01 20:23:58 -0300

    fix: login and register tests
    Co-authored-by: AlencarDiasDaCosta <AlencarDiasDaCosta@users.noreply.github.com>

--- back-end/src/tests/login.test.js
+++ back-end/src/tests/login.test.js
@@ -2,10 +2,10 @@ const sinon = require("sinon");
 
 const chai = require("chai");
 
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 
-const { app } = require('../api/app')
-const User = require('../database/models/User');
+const app = require("../api/app");
+const { User } = require("../database/models");
 
 chai.use(chaiHttp);
 
@@ -14,61 +14,47 @@ const { expect } = chai;
 //        password: '1c37466c159755ce1fa181bd247cb925' certa $#zebirita#$,
 // 'Cliente Zé Birita', 'zebirita@email.com', customer
 
-describe('Rota /Login', () => {
+describe("Rota /Login", () => {
 
-    beforeEach(async () => {
-        sinon
-          .stub(User, "findOne")
-          .resolves({
-            name: 'Cliente Zé Birita',
-            role: 'customer',
-            email: 'zebirita@email.com',
-            password: '1c37466c159755ce1fa181bd247cb925'
-          });
-      });
+  afterEach(() => sinon.restore());
 
-    it('quando o email está errrado', async () => {
-        const httpResponse = await chai
-           .request(app)
-           .post('/login')
-           .send({ email: 'zebirita@ea.com', password: '$#zebirita#$', })
-           expect(httpResponse.status).to.equal(404);
-           expect(httpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' });  
-      })
+  it("quando o email está errado", async () => {
+    sinon.stub(User, "findOne").resolves(null);
 
-      it('quando a senha está errrada', async () => {
-        const httpResponse = await chai
-           .request(app)
-           .post('/login')
-           .send({ email: 'zebirita@email.com', password: '$#zebir#$', })
-           expect(httpResponse.status).to.equal(404);
-           expect(httpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' });  
-      })
+    const httpResponse = await chai.request(app)
+    .post("/login")
+    .send({ email: "zebirita@ea.com", password: "$#zebirita#$" });
 
-      it('quando o email não está no formato correto', async () => {
-        const httpResponse = await chai
-           .request(app)
-           .post('/login')
-           .send({ email: 'zebirita@email', password: '$#zebirita#$', })
-           expect(httpResponse.status).to.equal(400);
-           expect(httpResponse.body).to.be.deep.equal({ message: 'Invalid email' });  
-      })
+    expect(httpResponse.status).to.equal(404);
+    expect(httpResponse.body).to.be.deep.equal({
+      message: "Incorrect email or password",
+    });
+  });
 
-      it('quando a senha não está no formato correto', async () => {
-        const httpResponse = await chai
-           .request(app)
-           .post('/login')
-           .send({ email: 'zebirita@email.com', password: '12345', })
-           expect(httpResponse.status).to.equal(400);
-           expect(httpResponse.body).to.be.deep.equal({ message: 'Invalid password' });  
-      })
+  it('quando a senha está errada', async () => {
+    sinon.stub(User, "findOne").resolves(null);
 
-      it('Login válido', async () => {
-        const httpResponse = await chai
-           .request(app)
-           .post('/login')
-           .send({ email: 'zebirita@email.com', password: '$#zebirita#$', })
-           expect(httpResponse.status).to.equal(200);
-           expect(httpResponse.body).to.have.property('token');  
-      })
-})
+    const httpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({ email: 'zebirita@email.com', password: '$#zebir#$', })
+       expect(httpResponse.status).to.equal(404);
+       expect(httpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+  })
+
+  it('Login válido', async () => {
+    sinon.stub(User, "findOne").resolves({
+      name: "Cliente Zé Birita",
+      role: "customer",
+      email: "zebirita@email.com",
+      password: "1c37466c159755ce1fa181bd247cb925",
+    });
+
+    const httpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({ email: 'zebirita@email.com', password: '$#zebirita#$', })
+       expect(httpResponse.status).to.equal(200);
+       expect(httpResponse.body).to.have.property('token');
+  })
+});
