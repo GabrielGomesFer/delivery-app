70b614e4406180bc1552ba7ac69b96285ac3d2dd
Aron Adams <aronrapetto@hotmail.com>
2022-12-06 18:15:25 -0300

    feat: middleware validation products

---
+++ back-end/src/middlwares/validationProducts.middleware.js
@@ -0,0 +1,14 @@
+const { Product } = require('../database/models');
+
+const verifyProducts = async (req, res, next) => {
+  const { productIds } = req.body;
+
+  const modelResult = await Promise.all(productIds
+    .map(async ({ productId }) => Product.findByPk(productId)));  
+  const products = modelResult.every((productResult) => productResult);
+
+  if (!products) res.status(404).json({ message: 'Some product not found' });
+  next();
+};
+
+module.exports = verifyProducts;
