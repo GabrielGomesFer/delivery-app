74b68fd8679f7e2bff388c6005fcf0569654f65c
Aron Adams <aronrapetto@hotmail.com>
2022-12-06 19:49:17 -0300

    feat: public images

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -9,6 +9,7 @@ const errorMiddleware = require('../middlwares/error.middleware');
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
