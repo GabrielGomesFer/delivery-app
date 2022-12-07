f301ae3834decf45d0b64cd9d634e370fa4bc213
Aron Adams <aronrapetto@hotmail.com>
2022-12-07 16:46:03 -0300

    feat: saleController updateSaleStatus

--- back-end/src/controller/sale.controller.js
+++ back-end/src/controller/sale.controller.js
@@ -14,7 +14,16 @@ const getSales = async (req, res) => {
   return res.status(200).json(sale);
 };
 
+const updateSaleStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  const updatedStatus = await saleService.updateSaleStatus(status, id);
+  return res.status(200).json(updatedStatus);
+};
+
 module.exports = {
   saleRegister,
   getSales,
+  updateSaleStatus,
 };
