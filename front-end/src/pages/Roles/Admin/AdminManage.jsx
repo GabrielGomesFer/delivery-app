682c7c0d9127e9f9e156a08bc9c5233bff3caf44
Aron Adams <aronrapetto@hotmail.com>
2022-12-10 01:53:51 -0300

    feat: UserTable render

--- front-end/src/pages/Roles/Admin/AdminManage.jsx
+++ front-end/src/pages/Roles/Admin/AdminManage.jsx
@@ -3,19 +3,26 @@ import Header from '../../../components/Header';
 import AddUserForm from '../../../components/Admin/AddUserForm/AddUserForm';
 import ERROR_TEST_ID from './data-testids';
 import { SAdminPageContainer } from './styles';
+import UserTable from '../../../components/Admin/UserTable/UserTable';
 
 function AdminManage() {
+  const [users, setUsers] = useState([]);
   const [displayError, setDisplayError] = useState({
     showError: false,
     message: '',
   });
+
   return (
     <SAdminPageContainer>
       <Header title="Produtos" url="/admin/manage" />
       { displayError.showError && (
         <span data-testid={ ERROR_TEST_ID }>{ displayError.message }</span>
       )}
-      <AddUserForm errorHandler={ { displayError, setDisplayError } } />
+      <AddUserForm
+        errorHandler={ { displayError, setDisplayError } }
+        usersTable={ { users, setUsers } }
+      />
+      <UserTable users={ users } setUsers={ setUsers } />
     </SAdminPageContainer>
   );
 }
