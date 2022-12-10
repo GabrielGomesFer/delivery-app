ee1c9aa4160f0975f8856e5ad3aafb975b483634
Aron Adams <aronrapetto@hotmail.com>
2022-12-10 00:19:03 -0300

    feat: Render AddUserFrom error element of admin page

--- front-end/src/pages/Roles/Admin/AdminManage.jsx
+++ front-end/src/pages/Roles/Admin/AdminManage.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+import Header from '../../../components/Header';
+import AddUserForm from '../../../components/Admin/AddUserForm/AddUserForm';
+import ERROR_TEST_ID from './data-testids';
+import { SAdminPageContainer } from './styles';
+
 function AdminManage() {
+  const [displayError, setDisplayError] = useState({
+    showError: false,
+    message: '',
+  });
   return (
-    <div>
-      <p>Tela de Admin</p>
-    </div>
+    <SAdminPageContainer>
+      <Header title="Produtos" url="/admin/manage" />
+      { displayError.showError && (
+        <span data-testid={ ERROR_TEST_ID }>{ displayError.message }</span>
+      )}
+      <AddUserForm errorHandler={ { displayError, setDisplayError } } />
+    </SAdminPageContainer>
   );
 }
 
