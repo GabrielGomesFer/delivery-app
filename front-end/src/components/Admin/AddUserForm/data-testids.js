8184e27b364517e1f010b079599b0dcf76b22462
Aron Adams <aronrapetto@hotmail.com>
2022-12-09 23:57:54 -0300

    feat: date-testids file

---
+++ front-end/src/components/Admin/AddUserForm/data-testids.js
@@ -0,0 +1,9 @@
+const addUserFormDataTestIds = {
+  inputName: 'admin_manage__input-name',
+  inputEmail: 'admin_manage__input-email',
+  inputPass: 'admin_manage__input-password',
+  inputRole: 'admin_manage__select-role',
+  registerButton: 'admin_manage__button-register',
+};
+
+export default addUserFormDataTestIds;
