3511bbe182a86713b4764a16fb4a314d35869b38
Aron Adams <aronrapetto@hotmail.com>
2022-12-13 04:31:40 -0300

    fix: order details page tests

--- front-end/src/tests/customer/customerOrderIDSale.test.js
+++ front-end/src/tests/customer/customerOrderIDSale.test.js
@@ -1,58 +1,69 @@
 import axios from 'axios';
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../helper/renderWithRouter';
 import App from '../../App';
 
 import userLS from '../mocks/userLSMock';
-import { saleOne } from '../mocks/saleIdMock';
+import saleOne from '../mocks/saleIdMock';
 
 jest.mock('axios');
 
 describe('Test Products', () => {
   describe('Testa funcionamento da página de produtos', () => {
     it('add 2, go to checkout and finish', async () => {
-      const { history } = renderWithRouter(<App />);
-      localStorage.setItem('user', JSON.stringify(userLS));
       axios.get.mockImplementation(async () => Promise.resolve(
         { data: saleOne },
       ));
 
+      localStorage.setItem('user', JSON.stringify(userLS));
+      const { history } = renderWithRouter(<App />);
+
       history.push('/customer/orders/1');
 
-      await Promise.all(saleOne.products.map(async ({ id }, i = 0) => {
-        const itemNumber = await screen
-          .findByTestId(`customer_order_details__element-order-table-item-number-${i}`);
+      const randomNumber = () => Math
+        .floor((Math.random() * saleOne.products.length));
 
-        const tableName = await screen
-          .findByTestId(`customer_order_details__element-order-table-name-${i}`);
+      const itemNumber = await screen
+        .findByTestId(
+          `customer_order_details__element-order-table-item-number-${randomNumber()}`,
+        );
 
-        const tableQuantity = await screen
-          .findByTestId(`customer_order_details__element-order-table-quantity-${i}`);
+      const tableName = await screen
+        .findByTestId(
+          `customer_order_details__element-order-table-name-${randomNumber()}`,
+        );
 
-        const tableUnitPrice = await screen
-          .findByTestId(`customer_order_details__element-order-table-unit-price-${i}`);
+      const tableQuantity = await screen
+        .findByTestId(
+          `customer_order_details__element-order-table-quantity-${randomNumber()}`,
+        );
 
-        const tableSubTotal = await screen
-          .findByTestId(`customer_order_details__element-order-table-sub-total-${i}`);
+      const tableUnitPrice = await screen
+        .findByTestId(
+          `customer_order_details__element-order-table-unit-price-${randomNumber()}`,
+        );
 
-        const totalPrice = await screen
-          .findByTestId('customer_order_details__element-order-total-price');
+      const tableSubTotal = await screen
+        .findByTestId(
+          `customer_order_details__element-order-table-sub-total-${randomNumber()}`,
+        );
 
-        // const deliverystatus = await screen
-        //   .findByTestId(
-        //     `customer_order_details__element-order-details-label-delivery-status-${id}`,
-        //   );
+      const totalPrice = await screen
+        .findByTestId('customer_order_details__element-order-total-price');
 
-        expect(itemNumber).toBeInTheDocument();
-        expect(tableName).toBeInTheDocument();
-        expect(tableQuantity).toBeInTheDocument();
-        expect(tableUnitPrice).toBeInTheDocument();
-        expect(tableSubTotal).toBeInTheDocument();
-        expect(totalPrice).toBeInTheDocument();
-        // expect(deliverystatus).toBeInTheDocument();
-      }));
+      const deliverystatus = await screen
+        .findByTestId(
+          'customer_order_details__element-order-details-label-delivery-status',
+        );
+
+      expect(itemNumber).toBeInTheDocument();
+      expect(tableName).toBeInTheDocument();
+      expect(tableQuantity).toBeInTheDocument();
+      expect(tableUnitPrice).toBeInTheDocument();
+      expect(tableSubTotal).toBeInTheDocument();
+      expect(totalPrice).toBeInTheDocument();
+      expect(deliverystatus).toBeInTheDocument();
 
       const orderId = await screen
         .findByTestId('customer_order_details__element-order-details-label-order-id');
